1e1a75385220bb7d5d869faf4d07d9146f8aa63f
VU LONG VU <longvuit18@gmail.com>
2022-11-18 13:33:23 +0700

    vuvl add darkmode

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import { PaletteMode } from '@mui/material';
 
 // Create a theme instance.
-const theme = createTheme({
+const theme = (mode: PaletteMode) => createTheme({
   palette: {
+    mode,
     primary: {
       main: '#556cd6',
     },
