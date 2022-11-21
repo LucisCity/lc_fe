39e357fa1d3ed95cbc5b91e1918e859960cbd4f8
loinv <loinv226@gmail.com>
2022-11-21 13:31:11 +0700

    Update: login google, fb

--- src/components/auth/login/hooks/use_login.ts
+++ src/components/auth/login/hooks/use_login.ts
@@ -1,6 +1,11 @@
 import Router from "next/router";
 import { useState } from "react";
 
+export enum SocialTye {
+  facebook = "facebook",
+  google = "google",
+}
+
 export default function useLogin() {
   const login = async function (email: string, password: string) {
     try {
@@ -16,7 +21,39 @@ export default function useLogin() {
     }
   };
 
+  function fbLogin(res: any) {
+    socialLogin(res, SocialTye.facebook);
+  }
+  function ggLogin(res: any) {
+    socialLogin(res, SocialTye.google);
+  }
+
+  async function socialLogin(res: any, type: SocialTye) {
+    try {
+      let input: any = {};
+      if (type == SocialTye.facebook) {
+        input.access_token = res?.accessToken;
+      } else if (type == SocialTye.google) {
+        input.web_access_token = res?.access_token ?? res?.code ?? res?.id_token;
+      }
+      if (Object.keys(input).length == 0) {
+        return;
+      }
+      Router.push("/");
+    } catch (err: any) {
+      // toast({
+      //   status: "error",
+      //   title: err.message ?? "Request failed",
+      //   isClosable: true,
+      // });
+    } finally {
+      // setSubmitting(false);
+    }
+  }
+
   return {
     login,
+    fbLogin,
+    ggLogin,
   };
 }
