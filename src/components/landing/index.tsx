e767a54cd3a94ec513a5fe8040e23dc082ee2c27
Vu Long Vu <vuvl@luciscity.io>
2022-11-24 15:31:46 +0700

    add scroll effect in mobile

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -42,6 +42,15 @@ export const LandingPage = () => {
     isPendingRef.current = data;
     setIsPending(data);
   };
+  /**
+   * Mobile only
+   */
+  const [initialY, setInitialY] = React.useState<number | null>(null);
+  const initialYRef = React.useRef(initialY);
+  const setInitialYRef = (data: number | null) => {
+    initialYRef.current = data;
+    setInitialY(data);
+  };
 
   // const scrollPosition = React.useMemo(() => {
   //   if (typeof window !== "undefined") {
@@ -54,10 +63,54 @@ export const LandingPage = () => {
 
   React.useEffect(() => {
     document.addEventListener("wheel", onScroll, { passive: false });
+    document.addEventListener(
+      "touchstart",
+      (e) => {
+        setInitialYRef(e.touches[0].clientY);
+      },
+      false,
+    );
+
+    document.addEventListener("touchmove", onTouchMove, { passive: false });
     document.body.style.overflow = "hidden";
     return () => document.removeEventListener("wheel", onScroll);
   }, []);
 
+  /**
+   * Mobile only
+   */
+  const onTouchMove = (event: TouchEvent) => {
+    console.log(2);
+    event.preventDefault();
+    if (initialYRef.current === null) {
+      return;
+    }
+    const currentY = event.touches[0].clientY;
+    const diffY = initialYRef.current - currentY;
+    console.log(diffY);
+    if (!isPendingRef.current) {
+      setIsPendingRef(true);
+      setTimeout(() => {
+        setIsPendingRef(false);
+      }, durationScroll);
+      if (diffY > 0) {
+        setActiveSection((pre) => {
+          if (pre > totalSection) {
+            return pre;
+          }
+          return pre + 1;
+        });
+      } else {
+        setActiveSection((pre) => {
+          if (pre === 0) {
+            return pre;
+          }
+          return pre - 1;
+        });
+      }
+    }
+    setInitialYRef(null);
+  };
   const onScroll = (event: WheelEvent) => {
     event.preventDefault();
     const scrollDown = (-event.deltaY || -event.detail) < 0;
