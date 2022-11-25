5a7e1882a065ec9047d99fa419c9ff0b6c199415
loinv <loinv226@gmail.com>
2022-11-26 04:04:46 +0700

    Remove unuse file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
-import Footer from "../components/layout/footer";
-import LandingHeader from "../components/layout/header/landing_header";
 import LandingPage from "../components/landing";
-import LandingPageV2 from "../components/landing/landing";
 
 const Home: NextPage = () => {
   return (
