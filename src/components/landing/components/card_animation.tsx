bd4653ac9c4ee7b46d6c83e37362740263cb1725
Vu Long Vu <vuvl@luciscity.io>
2022-11-30 16:50:44 +0700

    add new card section

--- src/components/landing/components/card_animation.tsx
+++ src/components/landing/components/card_animation.tsx
@@ -6,51 +6,27 @@ import AnimWhenVisible from "../../anim";
 import { usePaging } from "../../anim/swip_visible_anim";
 const ImageAnimation = styled(motion.img)(({ theme }) => ({
   position: "absolute",
-  height: 380,
+  height: 300,
+  width: "auto",
+  bottom: 0,
+
   [theme.breakpoints.down("md")]: {
-    height: 300,
-  },
-}));
-const Coin = styled("img")(({ theme }) => ({
-  position: "absolute",
-  right: -30,
-  top: -70,
-  zIndex: -1,
-  [theme.breakpoints.down("md")]: {
-    left: 200,
-    bottom: 50,
-  },
-  [theme.breakpoints.down("sm")]: {
-    display: "none",
+    height: 250,
   },
 }));
 
-const Coin2 = styled("img")(({ theme }) => ({
-  position: "absolute",
-  left: -50,
-  bottom: -50,
-  zIndex: -1,
-  [theme.breakpoints.down("md")]: {
-    left: 200,
-    bottom: 50,
-  },
-  [theme.breakpoints.down("sm")]: {
-    left: 70,
-    bottom: 50,
-  },
-}));
 const Wrapper = styled(motion.div)(({ theme }) => ({
   width: "100%",
   height: "100%",
   display: "flex",
   justifyContent: "center",
 
-  [theme.breakpoints.between("md", "lg")]: {
-    justifyContent: "flex-end",
-  },
-  [theme.breakpoints.down("md")]: {
-    paddingLeft: 50,
-  },
+  // [theme.breakpoints.between("md", "lg")]: {
+  //   justifyContent: "flex-end",
+  // },
+  // [theme.breakpoints.down("md")]: {
+  //   paddingLeft: 50,
+  // },
 }));
 
 interface IProps {
@@ -60,69 +36,60 @@ interface IProps {
 
 export const CardAnimation = (props: IProps) => {
   return (
-    <Box
-      position="relative"
-      width={"100%"}
-      height={380}
-      sx={(theme) => ({
-        [theme.breakpoints.between("sm", "md")]: {
-          marginTop: theme.spacing(32),
-        },
-        [theme.breakpoints.down("sm")]: {
-          marginTop: theme.spacing(16),
-        },
-      })}
-    >
+    <Box position="relative" width={"100%"} height={380} mt={{ xs: 20, sm: 30, md: 40 }}>
       <AnimWhenVisible
-        variants={{
-          visible: { opacity: 1, x: 0 },
-          hidden: { opacity: 0, x: -50, transition: { delay: 0 } },
-        }}
+        // variants={{
+        //   visible: { opacity: 1, y: -50 },
+        //   hidden: { opacity: 0, y: 0, transition: { delay: 0 } },
+        // }}
         // transition={{ delay: item.delay, duration: 0.6 }}
         index={props.animationIndex}
         style={{ width: "100%", height: "100%" }}
-        enable={props?.enable ?? true}
+        // enable={props?.enable ?? true}
+        enable={false}
       >
         <Wrapper
-          animate={{
-            y: [0, -20, 0],
-          }}
-          transition={{ ease: "linear", duration: 2, repeat: Infinity }}
+          animate={
+            {
+              // y: [0, -20, 0],
+            }
+          }
+          // transition={{ ease: "linear", duration: 2, repeat: Infinity }}
         >
           <>
             <ImageAnimation
-              src="/card.png"
+              src="/assets/imgs/landing/card2.png"
               style={{
-                zIndex: 3,
+                zIndex: 1,
               }}
             ></ImageAnimation>
             <ImageAnimation
-              src="/card.png"
+              src="/assets/imgs/landing/card2.png"
               style={{
                 zIndex: 2,
               }}
               animate={{
-                x: [0, -30],
-                rotate: [0, -10],
+                // x: [0, -30],
+                // rotate: [0, -10],
+                y: [0, -75],
               }}
               transition={{ ease: "linear", duration: 1 }}
             ></ImageAnimation>
             <ImageAnimation
-              src="/card.png"
+              src="/assets/imgs/landing/card3.png"
               style={{
-                zIndex: 1,
+                zIndex: 3,
               }}
               animate={{
-                x: [0, -60],
-                rotate: [0, -20],
+                // x: [0, -60],
+                // rotate: [0, -20],
+                y: [0, -150],
               }}
               transition={{ ease: "linear", duration: 1 }}
             ></ImageAnimation>
           </>
         </Wrapper>
       </AnimWhenVisible>
-      <Coin src="/assets/imgs/landing/coin3.png" alt="" />
-      <Coin2 src="/assets/imgs/landing/coin3.png" alt="" />
     </Box>
   );
 };
