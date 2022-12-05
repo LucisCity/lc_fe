26f7fe556e3b802ebc653dc4a5f4fe28661c2219
hiepnguyenba <nguyenbahiep001@gmail.com>
2022-12-06 06:37:00 +0700

    update debug page

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -1,37 +1,73 @@
-import type { NextPage } from "next";
+/* eslint-disable */
+import {useState} from "react";
+import type {NextPage} from "next";
+import { NoSsr, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import {appEnv, appVersionCommitId} from "../utils/env";
+import DocHead from "../components/layout/doc_head";
+import PageLayout from "../components/layout/PageLayout";
+import { ContactPage } from "../components/contact";
+import Button from "@mui/material/Button";
 
-// export async function getServerSideProps() {
-// 	// Fetch data from external API
-// 	const res = await fetch('https://dummyjson.com/products/1');
-// 	const data = await res.json();
-//
-// 	if (data !== '!!!!') {
-// 		throw new Error('Internal Server Error');
-// 	}
-// 	// Pass data to the page via props
-// 	return { props: { data } };
-// }
 
 const DebugPage: NextPage = () => {
+  const [errorComponentVisible, setErrorComponentVisible] = useState(false);
 
-	const handClick = () => {
-		undefinedFunc();
-	}
+  const makeAppCrash = () => {
+    setErrorComponentVisible(true);
+  }
 
-	return (
-		<section className="lucis-container" style={{marginTop: "120px"}}>
+  return (
+    <>
+      <DocHead />
+      <PageLayout>
+        <NoSsr>
+          <section className="lucis-container" style={{marginTop: "120px"}}>
+            <div>
+              <h1>Debug Page</h1>
+              <div>
+                <h3>App</h3>
+                <div>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center">Key</TableCell>
+                        <TableCell align="center">Value</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                          <TableCell align="center">Version (commit id)</TableCell>
+                          <TableCell align="center">{appVersionCommitId}</TableCell>
+                        </TableRow>
+                      <TableRow>
+                        <TableCell align="center">APP_ENV</TableCell>
+                        <TableCell align="center">{appEnv}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </div>
 
-			<div>
-				<h1>Debug Page</h1>
 
-				<div>
-					<h3>App</h3>
-					<button onClick={handClick}>Test 500 Error</button>
-				</div>
-
-			</div>
-		</section>
-	);
+                <h1>Error debug</h1>
+                <div>
+                  <Button variant="contained" onClick={makeAppCrash}>Test 500 Error</Button>
+                  {errorComponentVisible && <DebugErrorComponent />}
+                </div>
+              </div>
+            </div>
+          </section>
+        </NoSsr>
+      </PageLayout>
+    </>
+  );
 };
+
+function DebugErrorComponent() {
+  throw new Error("Force cause error");
+
+  return (
+    <h1>Crash</h1>
+  );
+}
 
 export default DebugPage;
