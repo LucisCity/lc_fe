d37c7401f0cbf5267417128420c95dd532685cd0
hiepnguyenba <nguyenbahiep001@gmail.com>
2022-12-06 03:58:58 +0700

    update debug page

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -1,28 +1,32 @@
 import type { NextPage } from "next";
 
-export async function getServerSideProps() {
-	// Fetch data from external API
-	const res = await fetch('https://dummyjson.com/products/1');
-	const data = await res.json();
-
-	if (data !== '!!!!') {
-		throw new Error('Internal Server Error');
-	}
-	// Pass data to the page via props
-	return { props: { data } };
-}
+// export async function getServerSideProps() {
+// 	// Fetch data from external API
+// 	const res = await fetch('https://dummyjson.com/products/1');
+// 	const data = await res.json();
+//
+// 	if (data !== '!!!!') {
+// 		throw new Error('Internal Server Error');
+// 	}
+// 	// Pass data to the page via props
+// 	return { props: { data } };
+// }
 
 const DebugPage: NextPage = () => {
 
+	const handClick = () => {
+		undefinedFunc();
+	}
+
 	return (
-		<section className="lucis-container">
+		<section className="lucis-container" style={{marginTop: "120px"}}>
 
 			<div>
 				<h1>Debug Page</h1>
 
 				<div>
 					<h3>App</h3>
-					<button>Test 500 Error</button>
+					<button onClick={handClick}>Test 500 Error</button>
 				</div>
 
 			</div>
