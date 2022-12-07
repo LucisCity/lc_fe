c87ebfc50a8574e4600946a48b2967ac7f3d9371
neo.mxn0 <neo.mxn0@gmail.com>
2022-12-07 17:26:35 +0700

    disable/enable card anim with page swiper

--- src/components/landing/components/card_animation.tsx
+++ src/components/landing/components/card_animation.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, {useCallback, useEffect} from "react";
 import { Box } from "@mui/material";
 import {AnimationControls, motion, Transition, useAnimation, Variants} from "framer-motion";
 import { styled, useTheme } from "@mui/system";
-// import AnimWhenVisible from "../../anim";
-// import { usePaging } from "../../anim/swip_visible_anim";
+import AnimWhenVisible from "../../anim";
+import { usePaging } from "../../anim/swip_visible_anim";
+import {isClientDevMode} from "../../../utils/env";
 const ImageAnimation = styled(motion.img)(({ theme }) => ({
   position: "absolute",
   height: 300,
@@ -37,22 +38,40 @@ interface IProps {
 export const CardAnimation = (props: IProps) => {
   const controls = useAnimation();
   const controls2 = useAnimation();
+  const paging = usePaging();
 
-  // const paging = usePaging();
-  // useEffect(() => {
-  //   if (props.enable === false) {
-  //     return;
-  //   }
-  //   if (props.animationIndex && paging.activeIndex) {
-  //     if (props.animationIndex === paging.activeIndex) {
-  //       controls.start("visible");
-  //     } else if (paging.activeIndex === props.animationIndex! - 1 && props.animationIndex === paging.preIndex) {
-  //       controls.start("hidden");
-  //     }
-  //     return;
-  //   }
-  //   controls.start("visible");
-  // }, [paging, props.enable]);
+  const start = useCallback(() => {
+    startSequence(controls, -75, -50)
+    startSequence(controls2, -150, -100)
+  }, [controls, controls2]);
+  const stop = useCallback(() => {
+    stopSequence(controls)
+    stopSequence(controls2)
+  }, [controls, controls2]);
+
+  useEffect(() => {
+    if (props.enable === false) return;
+
+    // Handle swiper slide change in landing page
+    if (props.animationIndex && paging.activeIndex) {
+      // scroll into page
+      if (props.animationIndex === paging.activeIndex) start();
+      // scroll out of page
+      else if (props.animationIndex === paging.preIndex) stop();
+    }
+    // in membership page
+    else start();
+
+    return () => stop()
+  }, [start, stop, paging, props.animationIndex, props.enable]);
+
+  async function stopSequence(controls: AnimationControls) {
+    await controls.start({
+      y: 0,
+      transition: {ease: "linear", duration: 1},
+    });
+    await controls.stop();
+  }
 
   async function startSequence(controls: AnimationControls, firstYOffset: number, repeatOffset: number) {
     await controls.start({
@@ -65,36 +84,30 @@ export const CardAnimation = (props: IProps) => {
     });
   }
 
-  useEffect(() => {
-    startSequence(controls, -75, -50)
-    startSequence(controls2, -150, -100)
-  }, []);
+  // Debug only
+  if (isClientDevMode) {
+    // @ts-ignore
+    // eslint-disable-next-line camelcase
+    window.tmp_stopSequence = stop;
+    // @ts-ignore
+    // eslint-disable-next-line camelcase
+    window.tmp_startSequence = start;
+  }
 
   return (
     <Box position="relative" width={"100%"} height={380} mt={{ xs: 20, sm: 30, md: 40 }}>
-      {/*<AnimWhenVisible*/}
-      {/*  index={props.animationIndex}*/}
-      {/*  style={{ width: "100%", height: "100%" }}*/}
-      {/*  // variants={{ visible: { scale: 1, y: [0, 0] }, hidden: { scale: 2, y: [-100, 0] } }}*/}
-      {/*>*/}
-      {/*  <Wrapper animate={{ y: [0, -10, 0] }} transition={{ ease: "linear", duration: 2, repeat: Infinity }}>*/}
-      <div>
-          <ImageAnimation src="/assets/imgs/landing/card2.png" style={{ zIndex: 1 }}></ImageAnimation>
-          <ImageAnimation
-            src="/assets/imgs/landing/card2.png"
-            style={{ zIndex: 2 }}
-            animate={controls}
-            // variants={{ visible: { y: [0, -75] }, hidden: { y: [0, 0] } }}
-            // transition={{ ease: "linear", duration: 1 }}
-          ></ImageAnimation>
-          <ImageAnimation
-            src="/assets/imgs/landing/card3.png"
-            style={{ zIndex: 3 }}
-            animate={controls2}
-            // variants={{ visible: { y: [0, -150] }, hidden: { y: [0, 0] } }}
-            // transition={{ ease: "linear", duration: 1 }}
-          ></ImageAnimation>
-      </div>
+      <AnimWhenVisible
+        index={props.animationIndex}
+        style={{ width: "100%", height: "100%" }}
+        variants={{
+          visible: { opacity: 1, y: 0 },
+          hidden: { opacity: 0, y: 100, transition: { delay: 0 } },
+        }}
+      >
+        <ImageAnimation src="/assets/imgs/landing/card2.png" style={{ zIndex: 1 }}></ImageAnimation>
+        <ImageAnimation src="/assets/imgs/landing/card2.png" style={{ zIndex: 2 }} animate={controls} />
+        <ImageAnimation src="/assets/imgs/landing/card3.png" style={{ zIndex: 3 }} animate={controls2} />
+      </AnimWhenVisible>
     </Box>
   );
 };
