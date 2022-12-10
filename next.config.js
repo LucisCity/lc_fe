26ec9b3547145a03967e7ae2749b5158088a87be
neo.mxn0 <neo.mxn0@gmail.com>
2022-12-10 21:20:36 +0700

    PWA: Add splash screen & change logo

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ const defaultRuntimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
   dest: 'public',
   // Disable PWA in dev mode, if you need to debug PWA in dev mode, plz turn it on
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
   /*
     Force next-pwa to generate worker box production build by specify the option mode: 'production' in your pwa section of next.config.js. Though next-pwa automatically generate the worker box development build during development (by running next) and worker box production build during production (by running next build and next start). You may still want to force it to production build even during development of your web app for following reason:
     Reduce logging noise due to production build doesn't include logging.
-    Improve performance a bit due to production build is optimized and minified.
+    Improve performance a bit duOe to production build is optimized and minified.
      */
-  // mode: 'production', // Disable this if you want to debug PWA in dev mode
+  mode: 'production', // Disable this if you want to debug PWA in dev mode
 
   publicExcludes: [
     '!version.json', // try to avoid version.json cache
