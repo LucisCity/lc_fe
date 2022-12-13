c8b6218e0a3104d75765e029e305defe839469ba
hiepbanguyen <nguyenbahiep001@gmail.com>
2022-12-13 18:58:53 +0700

    fix apple ui

--- src/components/profile/components/navbar/navbar.tsx
+++ src/components/profile/components/navbar/navbar.tsx
@@ -25,7 +25,9 @@ const Tab = (props: TabProps) => {
   return (
     <Button
       className={`
-        ${props.logoutButton ? props.topLogoutButton ? s.topLogoutButton : s.logoutButton : props.active ? s.active : s.default}
+        ${props.logoutButton ?
+        (props.topLogoutButton ? s.topLogoutButton : s.bottomLogoutButton) :
+        (props.active ? s.active : s.default)}
       `}
       href={props.href}
       LinkComponent={Link}
@@ -148,7 +150,6 @@ export const ProfileNavBar = (props: ProfileNavBarProps) => {
         >
         </Box>
         <Box
-          container
           sx={{
             display: "flex",
             flexDirection: "column",
@@ -259,7 +260,6 @@ export const ProfileNavBar = (props: ProfileNavBarProps) => {
                   logoutButton={true}
                 />
               </Stack>
-
             </Grid>
           </Grid>
         </Box>
