a780de0fc77282c3264d58daa7be300075d21ab6
neo.mxn0 <neo.mxn0@gmail.com>
2022-12-14 21:10:52 +0700

    Profile/App: UI, UX improvement

--- src/components/common/day_night_switch/index.tsx
+++ src/components/common/day_night_switch/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import s from './index.module.css'
 
-type Props = { toggled: boolean, onClick: any }
+type Props = { dark: boolean, onClick: any }
 export default function DayNightSwitch(props: Props) {
   return (
-    <div onClick={props.onClick} className={`${s.toggle} ${props.toggled ? s.night : ""}`}>
+    <div onClick={props.onClick} className={`${s.toggle} ${props.dark ? s.night : ""}`}>
       <div className={s.notch}>
         <div className={s.crater} />
         <div className={s.crater} />
