e562ec83738054a2732861991309e84120dc9859
neo.mxn0 <neo.mxn0@gmail.com>
2022-12-14 18:45:20 +0700

    Add Day Night Switch

---
+++ src/components/common/day_night_switch/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from './index.module.css'
+
+type Props = { toggled: boolean, onClick: any }
+export default function DayNightSwitch(props: Props) {
+  return (
+    <div onClick={props.onClick} className={`${s.toggle} ${props.toggled ? s.night : ""}`}>
+      <div className={s.notch}>
+        <div className={s.crater} />
+        <div className={s.crater} />
+      </div>
+      <div>
+        <div className={`${s.shape} ${s.sm}`} />
+        <div className={`${s.shape} ${s.sm}`} />
+        <div className={`${s.shape} ${s.md}`} />
+        <div className={`${s.shape} ${s.lg}`} />
+      </div>
+    </div>
+  );
+}
