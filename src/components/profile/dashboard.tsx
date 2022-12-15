c57276cad3eae7c99af73cf1ecce61c44877d708
loinv <loinv226@gmail.com>
2022-12-15 18:04:45 +0700

    Update: handle unauth router

--- src/components/profile/dashboard.tsx
+++ src/components/profile/dashboard.tsx
@@ -2,7 +2,9 @@
 import { Box } from "@mui/system";
 import { Grid, Paper, Typography } from "@mui/material";
 import CustomPaginationActionsTable from "./components/table";
-import React from "react";
+import React, { useEffect } from "react";
+import { useStores } from "../../store";
+import Router from "next/router";
 
 const tutorialStepData = [
   {
@@ -24,29 +26,23 @@ const DashBoardItem = () => {
     <>
       {tutorialStepData.map((item) => (
         <React.Fragment key={item.title}>
-          <Grid item xs={4} height={"inherit"} my={{md: 2}}>
+          <Grid item xs={4} height={"inherit"} my={{ md: 2 }}>
             <Paper
               elevation={item.title == "Tổng tài sản ước tính" ? 5 : 0}
               sx={{
                 borderRadius: 4,
-                px: {sm: 4, xs: 7},
+                px: { sm: 4, xs: 7 },
                 // pl: {md: 4, sm: 4, xs: 1},
-                py: {md: 7, xs: 3},
+                py: { md: 7, xs: 3 },
                 height: "100%",
                 background: `${item.title == "Tổng tài sản ước tính" ? "#6555EE" : "#fff"}`,
-                color: `${item.title == "Tổng tài sản ước tính" ? "#fff" : '#504C67'}`,
+                color: `${item.title == "Tổng tài sản ước tính" ? "#fff" : "#504C67"}`,
               }}
             >
-              <Typography
-                fontSize={{lg: 16, md: 13.5, xs: 16}}
-                fontWeight={500}>
+              <Typography fontSize={{ lg: 16, md: 13.5, xs: 16 }} fontWeight={500}>
                 {item.title}
               </Typography>
-              <Typography
-                fontSize={{md: 30, xs: 20}}
-                fontWeight={600}
-                my={{lg: 4, sm: 2, xs: 0}}
-              >
+              <Typography fontSize={{ md: 30, xs: 20 }} fontWeight={600} my={{ lg: 4, sm: 2, xs: 0 }}>
                 {item.content}
               </Typography>
             </Paper>
@@ -58,21 +54,26 @@ const DashBoardItem = () => {
 };
 
 export const ProfileDashboard = () => {
+  const { userStore } = useStores();
+
+  useEffect(() => {
+    if (!userStore.isLogedIn) {
+      Router.push("/login");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Box mx={{sm: 10, xs: 3}} my={7}>
-      <Typography
-        fontWeight={700}
-        fontSize={{sm: 32, xs: 25}}
-        textAlign={{sm: "left", xs: "center"}}
-      >
+    <Box mx={{ sm: 10, xs: 3 }} my={7}>
+      <Typography fontWeight={700} fontSize={{ sm: 32, xs: 25 }} textAlign={{ sm: "left", xs: "center" }}>
         Dashboard
       </Typography>
-      <Box mt={5} mb={3} mx={{sm: 0, xs: 6}}>
-        <Grid container direction={{sm: "row", xs: "column"}} spacing={3} height={"auto"}>
-          <DashBoardItem/>
+      <Box mt={5} mb={3} mx={{ sm: 0, xs: 6 }}>
+        <Grid container direction={{ sm: "row", xs: "column" }} spacing={3} height={"auto"}>
+          <DashBoardItem />
         </Grid>
       </Box>
-      <CustomPaginationActionsTable/>
+      <CustomPaginationActionsTable />
     </Box>
-  )
-}
+  );
+};
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,26 +1,35 @@
 /* eslint-disable */
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, Grid } from "@mui/material";
 import { ProfileNavBar } from "./components/navbar/navbar";
 import { Background } from "../common/background/background";
 import ScrollPage from "../layout/scroll_page";
 import { Container } from "@mui/system";
 import { useRouter } from "next/router";
-import s from "./index.module.sass"
+import s from "./index.module.sass";
+import { useStores } from "../../store";
 
 type Props = {
   children: any;
 };
 
 export const ProfileLayout = (props: Props) => {
-
+  const { userStore } = useStores();
   const router = useRouter();
-  const activeTab = router.query.tab ? `/profile/${router.query.tab}` : '/profile';
+
+  useEffect(() => {
+    if (!userStore.isLogedIn) {
+      router.push("/login");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const activeTab = router.query.tab ? `/profile/${router.query.tab}` : "/profile";
   // console.log('active tab href', activeTab);
 
   return (
     <ScrollPage>
-      <Background/>
+      <Background />
       <Container component="div" className={s.profileC}>
         <Card
           sx={{
@@ -34,26 +43,31 @@ export const ProfileLayout = (props: Props) => {
             borderLeft: "1px solid #fff",
             borderTop: "1px solid #fff",
             borderBottom: "1px solid #fff",
-            borderRight: {md: "none", xs: "1px solid #fff"},
+            borderRight: { md: "none", xs: "1px solid #fff" },
           }}
           elevation={0}
         >
           <Grid container>
-            <Grid item sm={3} xs={12} sx={{
-              WebkitBackfaceVisibility: "hidden",
-              MozBackfaceVisibility: "hidden",
-              WebkitTransform: "translate3d(0, 0, 0)",
-              MozTransform: "translate3d(0, 0, 0)",
-            }}>
-              <ProfileNavBar activeTab={activeTab}/>
+            <Grid
+              item
+              sm={3}
+              xs={12}
+              sx={{
+                WebkitBackfaceVisibility: "hidden",
+                MozBackfaceVisibility: "hidden",
+                WebkitTransform: "translate3d(0, 0, 0)",
+                MozTransform: "translate3d(0, 0, 0)",
+              }}
+            >
+              <ProfileNavBar activeTab={activeTab} />
             </Grid>
             <Grid
               item
               sm={9}
               xs={12}
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
+                display: "flex",
+                flexDirection: "column",
                 backgroundColor: "#f9f9f9",
 
                 WebkitBackfaceVisibility: "hidden",
