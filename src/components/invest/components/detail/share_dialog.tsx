431f165cc74275f984fc497d331ab132995b3b38
loinv <loinv226@gmail.com>
2022-12-15 15:02:59 +0700

    Update: google map

--- src/components/invest/components/detail/share_dialog.tsx
+++ src/components/invest/components/detail/share_dialog.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 import { Box, Typography } from "@mui/material";
@@ -50,9 +47,6 @@ export default function ShareDialog() {
 
   return (
     <>
-      {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Slide in alert dialog
-      </Button> */}
       <Button
         variant="contained"
         endIcon={<Box component="img" src="/assets/imgs/invest/icons/ic_share.svg" alt="" />}
