0f17341bcf5ad99ae0a0a3d08d7d4e0fd381d945
hiepbanguyen <nguyenbahiep001@gmail.com>
2022-12-20 16:34:56 +0700

    add pagination for list type content, update some ui

--- src/store/user.store.ts
+++ src/store/user.store.ts
@@ -18,8 +18,8 @@ class UserStore {
     return this._token;
   }
   get isLoggedIn() {
-    // return this._token != null && this._token != "";
-    return true;
+    return this._token != null && this._token != "";
+    // return true;
   }
 
   get isLoadedFromLocal() {
