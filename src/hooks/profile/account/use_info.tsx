b3da7972bfaac6ab4cc39fa4739f5bec0d4b37d0
hiepbanguyen <nguyenbahiep001@gmail.com>
2022-12-22 17:35:13 +0700

    add update profile

--- src/hooks/profile/account/use_info.tsx
+++ src/hooks/profile/account/use_info.tsx
@@ -1,5 +1,9 @@
-import { ApolloError, ApolloQueryResult, gql, useQuery } from "@apollo/client";
-import { AccountInfo } from "../../../gql/graphql";
+import { ApolloError, ApolloQueryResult, gql, useMutation, useQuery } from "@apollo/client";
+import { AccountInfo, AccountInfoUpdateInput, MutationUpdateAccountInfoArgs } from "../../../gql/graphql";
+import { isEmpty } from "lodash";
+import { useSnackbar } from "notistack";
+import { handleGraphqlErrors } from "../../../utils/apolo.util";
+import UserStore from "../../../store/user.store";
 
 export const GET_ACCOUNT_INFO = gql`
   query {
@@ -26,5 +30,44 @@ export function useGetAccountInfo(): {
     loadingAccountInfo: loading,
     errorAccountInfo: error,
     dataAccountInfo: data?.getAccountInfo,
+  };
+}
+
+export const UPDATE_ACCOUNT_INFO = gql`
+  mutation ($input: AccountInfoUpdateInput!) {
+    updateAccountInfo(input: $input)
+  }
+`;
+
+type UseUpdateAccountInfoProps = {
+  input: AccountInfoUpdateInput;
+  onCompleted?: (data: any) => void;
+};
+
+export function useUpdateAccountInfo({ input, onCompleted }: UseUpdateAccountInfoProps): {
+  updateAccountInfo: () => Promise<any>;
+  accountInfoUpdating: boolean;
+  dataUpdateAccountInfo: any;
+  errorUpdateAccountInfo: ApolloError | undefined;
+} {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [
+    updateAccountInfo,
+    { loading: accountInfoUpdating, data: dataUpdateAccountInfo, error: errorUpdateAccountInfo },
+  ] = useMutation(UPDATE_ACCOUNT_INFO, {
+    onCompleted: onCompleted,
+    onError: (e) => {
+      const errors = handleGraphqlErrors(e);
+      errors.forEach((err) => enqueueSnackbar(err.message, { variant: "error" }));
+    },
+    fetchPolicy: "no-cache",
+  });
+
+  return {
+    updateAccountInfo,
+    accountInfoUpdating,
+    dataUpdateAccountInfo,
+    errorUpdateAccountInfo,
   };
 }
