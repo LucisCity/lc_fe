469008c8ec7ea60dc4eb358eb4c5ca3ed58cd4bd
loinv <loinv226@gmail.com>
2022-12-23 14:37:57 +0700

    Update: connect btn

--- src/pages/wc.tsx
+++ src/pages/wc.tsx
@@ -1,8 +1,13 @@
 import { Box } from "@mui/material";
 import type { NextPage } from "next";
+import ConnectWalletButton from "../components/profile/account/wallet/components/connect_wallet_button";
 
 const Member: NextPage = () => {
-  return <Box></Box>;
+  return (
+    <Box minHeight="100vh">
+      <ConnectWalletButton />
+    </Box>
+  );
 };
 
 export default Member;
