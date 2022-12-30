d868e1a3cbf4d784549bc9ae4fad6345a541c76c
Vu Long Vu <vuvl@luciscity.io>
2022-12-30 14:06:00 +0700

    vuvl fix subscription

--- src/utils/apolo.util.ts
+++ src/utils/apolo.util.ts
@@ -74,7 +74,7 @@ if (isClient) {
     createClient({
       url: process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL ?? "",
       connectionParams: {
-        Authorization: `Bearer ${_getAuthToken()}`,
+        authorization: `Bearer ${_getAuthToken()}`,
       },
     }),
   );
