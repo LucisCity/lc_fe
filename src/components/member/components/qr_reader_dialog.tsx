f0618af648583e97bf444dcf90b97318d2bfd311
hiepbanguyen <nguyenbahiep001@gmail.com>
2023-01-11 13:22:56 +0700

    update qr reader

--- src/components/member/components/qr_reader_dialog.tsx
+++ src/components/member/components/qr_reader_dialog.tsx
@@ -21,10 +21,56 @@ const Transition = React.forwardRef(function Transition(
 
 type DialogContent = "QRReader" | "NotFound" | "PermissionDenied" | "Others" | "Loading";
 
-const QRReader = ({ open }: { open: boolean }) => {
+const QRReader = () => {
   const [result, setResult] = React.useState("No result");
   const [_, copy] = useCopyToClipboard();
   const { enqueueSnackbar } = useSnackbar();
+  const onCopy = () => {
+    copy(result);
+    enqueueSnackbar("Copied", { variant: "success" });
+  };
+
+  // if (!open) return null;
+  return (
+    <>
+      <Typography variant="h5" mb="20px">
+        Quét mã QR
+      </Typography>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setResult(result?.getText());
+          }
+        }}
+        constraints={{ facingMode: "environment" }}
+        containerStyle={{ width: "100%" }}
+        videoContainerStyle={{ width: "100%" }}
+        videoStyle={{ width: "100%" }}
+      />
+      <Typography
+        variant="body1"
+        color="primary"
+        textAlign={"center"}
+        sx={{ mt: 5, width: { md: "80%", xs: "95%" } }}
+        style={{ wordWrap: "break-word" }}
+      >
+        {result}
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ mt: "8px" }}
+        onClick={() => {
+          onCopy();
+        }}
+      >
+        Copy
+      </Button>
+    </>
+  );
+};
+
+export default function QRReaderDialog() {
+  const [open, setOpen] = React.useState(false);
   const [dialogContent, setDialogContent] = React.useState<DialogContent>("Loading");
   const getUserMedia = React.useCallback(async () => {
     await navigator.mediaDevices
@@ -49,88 +95,9 @@ const QRReader = ({ open }: { open: boolean }) => {
         }
       });
   }, []);
-  React.useEffect(() => {
-    if (open) {
-      getUserMedia();
-    }
-  });
-  const onCopy = () => {
-    copy(result);
-    enqueueSnackbar("Copied", { variant: "success" });
-  };
-
-  if (!open) return null;
-  return (
-    <>
-      {dialogContent === "Loading" && <CircularProgress color="secondary" size={100} />}
-      {dialogContent === "QRReader" && (
-        <>
-          {" "}
-          <Typography variant="h5" mb="20px">
-            Quét mã QR
-          </Typography>
-          <QrReader
-            onResult={(result, error) => {
-              if (!!result) {
-                setResult(result?.getText());
-              }
-
-              if (!!error) {
-                console.info(error);
-              }
-            }}
-            constraints={{ facingMode: "environment" }}
-            containerStyle={{ width: "100%" }}
-            videoContainerStyle={{ width: "100%" }}
-            videoStyle={{ width: "100%" }}
-          />
-          <Typography
-            variant="body1"
-            color="primary"
-            textAlign={"center"}
-            sx={{ mt: 5, width: { md: "80%", xs: "95%" } }}
-            style={{ wordWrap: "break-word" }}
-          >
-            {result}
-          </Typography>
-          <Button
-            variant="contained"
-            sx={{ mt: "8px" }}
-            onClick={() => {
-              onCopy();
-            }}
-          >
-            Copy
-          </Button>
-        </>
-      )}
-      {dialogContent === "NotFound" && (
-        <Typography variant={"h4"}>
-          Không thể tìm thấy camera để thực hiện quét QR, vui lòng chuyển sang thiết bị có camera để trải nghiệm chức
-          năng này
-        </Typography>
-      )}
-      {dialogContent === "PermissionDenied" && (
-        <>
-          <Typography variant={"h4"}>
-            Quyền truy cập camera bị từ chối, làm theo hướng dẫn bên dưới để cho phép quyền truy cập
-          </Typography>
-          <Box component={"img"} src={"/assets/imgs/member/camera_denied.png"} width={"95%"} mt={3} />
-        </>
-      )}
-      {dialogContent === "Others" && (
-        <Typography variant={"h4"}>
-          Trình duyệt gặp phải sự cố, vui lòng chuyển sang thiết bị khác có camera để trải nghiệm chức năng này
-        </Typography>
-      )}
-    </>
-  );
-};
-
-export default function QRReaderDialog() {
-  const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = async () => {
+    await getUserMedia();
     setOpen(true);
   };
   const handleClose = () => {
@@ -161,7 +128,29 @@ export default function QRReaderDialog() {
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogContent sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <QRReader open={open} />
+          <>
+            {dialogContent === "Loading" && <CircularProgress color="secondary" size={100} />}
+            {dialogContent === "QRReader" && <QRReader />}
+            {dialogContent === "NotFound" && (
+              <Typography variant={"h4"}>
+                Không thể tìm thấy camera để thực hiện quét QR, vui lòng chuyển sang thiết bị có camera để trải nghiệm
+                chức năng này
+              </Typography>
+            )}
+            {dialogContent === "PermissionDenied" && (
+              <>
+                <Typography variant={"h4"}>
+                  Quyền truy cập camera bị từ chối, làm theo hướng dẫn bên dưới để cho phép quyền truy cập
+                </Typography>
+                <Box component={"img"} src={"/assets/imgs/member/camera_denied.png"} width={"95%"} mt={3} />
+              </>
+            )}
+            {dialogContent === "Others" && (
+              <Typography variant={"h4"}>
+                Trình duyệt gặp phải sự cố, vui lòng chuyển sang thiết bị khác có camera để trải nghiệm chức năng này
+              </Typography>
+            )}
+          </>
         </DialogContent>
       </Dialog>
     </>
